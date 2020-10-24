4063cf000e379136d271f834830051a49574c59c
kasirajan22 <kasi.srm@gmail.com>
2020-10-24 06:08:38 +0530

    Initial Commit

---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Magizhan on 24/10/20.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
